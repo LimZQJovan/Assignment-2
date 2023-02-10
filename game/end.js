d3bf0521dee0fa9099e88adbecfef24058e5092b
Lee Royston <S10247568@connect.np.edu.sg>
2023-02-10 22:32:45 +0800

    API

--- game/end.js
+++ game/end.js
@@ -1,13 +1,11 @@
-const username = document.querySelector('#username') 
-const saveScoreBtn = document.querySelector('#saveScoreBtn') 
-const finalScore = document.querySelector('#finalScore')
-const mostRecentScore = localStorage.getItem('mostRecentScore')
+// const username = document.querySelector('#username') 
+// const saveScoreBtn = document.querySelector('#saveScoreBtn') 
+// const finalScore = document.querySelector('#finalScore')
+// const mostRecentScore = localStorage.getItem('mostRecentScore')
+// // const MAX_HIGH_SCORES = 5
+// const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
-
-const MAX_HIGH_SCORES = 5
-
-finalScore.innerText = mostRecentScore
+// finalScore.innerText = mostRecentScore
 
 username.addEventListener('keyup', () => { 
     saveScoreBtn.disabled = !username.value
@@ -32,3 +30,47 @@ saveHighScore = e => {
     localStorage.setItem('highScores', JSON.stringify(highScores))
     window.location.assign('../index.html')
 }
+
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-analytics.js";
+
+// const firebaseConfig = {
+// apiKey: "AIzaSyDaPtDKd2-ogeeeAI-EJmGULRjAL47usr4",
+// authDomain: "id-assgn-2.firebaseapp.com",
+// databaseURL: "https://id-assgn-2-default-rtdb.asia-southeast1.firebasedatabase.app",
+// projectId: "id-assgn-2",
+// storageBucket: "id-assgn-2.appspot.com",
+// messagingSenderId: "410083931076",
+// appId: "1:410083931076:web:c34e6c7c5c2c0649d5aa91",
+// measurementId: "G-0FQ3F8J01D"
+// };
+
+// //  Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// import { getDatabase, ref, set, child, get, update, remove }
+//     from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+
+// const db = getDatabase();
+// const username = document.querySelector('#username') 
+// const saveScoreBtn = document.querySelector('#saveScoreBtn') 
+// const finalScore = document.querySelector('#finalScore')
+// const mostRecentScore = localStorage.getItem('mostRecentScore')
+
+
+// finalScore.innerText = mostRecentScore;
+
+// saveHighScore = e => {
+//     e.preventDefault()
+
+//     // Store the high score
+//     db.ref("highscore").push({
+//         name: username.value,
+//         highscore: mostRecentScore
+//     });
+
+//     window.location.assign('../index.html')
+// }
--- highscores/highscores.js
+++ highscores/highscores.js
@@ -9,3 +9,48 @@ highScores.map(score => {
 //To remove all the players from the leaderboard
 // localStorage.removeItem('highScores');
 // highScoresList.innerHTML = '';
+
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-analytics.js";
+
+// const firebaseConfig = {
+// apiKey: "AIzaSyDaPtDKd2-ogeeeAI-EJmGULRjAL47usr4",
+// authDomain: "id-assgn-2.firebaseapp.com",
+// databaseURL: "https://id-assgn-2-default-rtdb.asia-southeast1.firebasedatabase.app",
+// projectId: "id-assgn-2",
+// storageBucket: "id-assgn-2.appspot.com",
+// messagingSenderId: "410083931076",
+// appId: "1:410083931076:web:c34e6c7c5c2c0649d5aa91",
+// measurementId: "G-0FQ3F8J01D"
+// };
+
+//  // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// import { getDatabase, ref, set, child, get, update, remove }
+//     from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+
+// const db = getDatabase();
+
+// // Get a reference to the highscore child of the database
+// var highscoreRef = db.ref("highscore");
+
+// // Use the `once()` method to retrieve the data
+// highscoreRef.once("value", function(snapshot) {
+//   // Get the highscore data from the snapshot
+//   var highscore = snapshot.val();
+
+//   // Get the username from the highscore data
+//   var username = highscore.name;
+
+//   // Get the actual highscore
+//   var highscore = highscore.score;
+
+//   highScoresList.innerHTML =
+//   highScores.map(highscore => {
+//       return `<li class="high-score">${username} ${highscore}</li>`
+//   }).join('')
+// });
